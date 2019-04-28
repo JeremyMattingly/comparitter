c72ccc8f6c05d77d19b9d3f22ea4923c42ec77f6
Jeremy Mattingly <jm@jeremymattingly.com>
2019-04-28 18:17:57 -0500

    Added new web project. Using WebForms just because it will be the quickest for me.

---
+++ Comparitter/Comparitter.Web/Compare.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Comparitter.Web
+{
+    public partial class Compare : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CopmareBtn_Click(object sender, EventArgs e)
+        {
+            string word1 = Word1Tb.Text;
+            string word2 = Word2Tb.Text;
+
+            var result = Comparitter.Compare.Compare.CompareByAppearanceCount(word1, word2);
+
+            ResultsLbl.Text = result.SearchElapsedSeconds.ToString();
+        }
+    }
+}
