e82ac159a3c690c28444cb6f5c6a161aa43e46b2
Jeremy Mattingly <jm@jeremymattingly.com>
2019-04-28 18:33:50 -0500

    Replaced Default page.

--- Comparitter/Comparitter.Web/Default.aspx.cs
+++ Comparitter/Comparitter.Web/Default.aspx.cs
@@ -7,11 +7,21 @@ using System.Web.UI.WebControls;
 
 namespace Comparitter.Web
 {
-    public partial class _Default : Page
+    public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void CopmareBtn_Click(object sender, EventArgs e)
+        {
+            string word1 = Word1Tb.Text;
+            string word2 = Word2Tb.Text;
+
+            var result = Comparitter.Compare.Compare.CompareByAppearanceCount(word1, word2);
+
+            ResultsLbl.Text = result.SearchElapsedSeconds.ToString();
         }
     }
 }
